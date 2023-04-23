0f2d8090f7f7cd4202a7b293a63f23bb91451ecf
iivanov21 <iivanov21@hotmail.com>
2023-04-23 14:47:07 +0100

    New environment packages
    -A* pathfinding removed

--- Assets/Scripts/EnemyGraphicsEffects.cs
+++ Assets/Scripts/EnemyGraphicsEffects.cs
@@ -1,23 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
+//using Pathfinding;
 public class EnemyGraphicsEffects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public AIPath m_AiPath;
+    //// Start is called before the first frame update
+    //public AIPath m_AiPath;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_AiPath.desiredVelocity.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        }
-        else if(m_AiPath.desiredVelocity.x<=-0.01f)
-        {
-            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (m_AiPath.desiredVelocity.x >= 0.01f)
+    //    {
+    //        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+    //    }
+    //    else if(m_AiPath.desiredVelocity.x<=-0.01f)
+    //    {
+    //        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
-        }
-    }
+    //    }
+    //}
 }
